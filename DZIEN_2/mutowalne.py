651e5e90cd593816790137789b8e57bf459d7c1b
albim72 <marcin.albiniak72@gmail.com>
2025-09-09 14:25:36 +0200

    Update mutowalne.py

--- DZIEN_2/mutowalne.py
+++ DZIEN_2/mutowalne.py
@@ -7,7 +7,27 @@ def znajdz_i_zamien(imiona):
             wynik.append(imie.upper())
     return wynik
 
-wynik = znajdz_i_zamien(imiona)
+wynik_c = znajdz_i_zamien(imiona)
 print(len(imiona))
+print(wynik_c)
+print(len(wynik_c))
+
+#przypadek 2
+print("________ przypadek modyfikacji globalnej _________")
+wynik2 = []
+def znajdz_i_zamien2(imiona):
+    for imie in imiona:
+        if imie.startswith("A"):
+            wynik2.append(imie.upper())
+
+znajdz_i_zamien2(imiona)
+print(len(imiona))
+print(wynik2)
+print(len(wynik2))
+
+wynik2.append("Leon")
+print(wynik2)
+
+wynik.append("Tomasz")
 print(wynik)
-print(len(wynik))
+
