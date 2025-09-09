5b70fec6c5a5e3d88ec9c34c91b95bf8e31ad59a
albim72 <marcin.albiniak72@gmail.com>
2025-09-09 13:41:05 +0200

    Update p1.py

--- DZIEN_2/zlamanie_paradygmatu_f/p1.py
+++ DZIEN_2/zlamanie_paradygmatu_f/p1.py
@@ -5,3 +5,16 @@ def add_item(item, bag=[]):      # ← domyślna lista ŻYJE między wywołaniam
 print(add_item("miecz"))         # ['miecz']
 print(add_item("tarcza"))        # ['miecz', 'tarcza']  ← poprzedni stan wrócił z zaświatów
 print(add_item("eliksir"))       # ['miecz', 'tarcza', 'eliksir']
+
+print(add_item("miecz"))
+print(add_item("proca"))
+
+#rozwiązanie bezpieczne
+def add_item_safe(item, bag=None):
+    bag = [] if bag is None else list(bag)
+    bag.append(item)
+    return bag
+
+print(add_item_safe("miecz"))         # ['miecz']
+print(add_item_safe("tarcza"))        # ['miecz', 'tarcza']  ← poprzedni stan wrócił z zaświatów
+print(add_item_safe("eliksir"))
