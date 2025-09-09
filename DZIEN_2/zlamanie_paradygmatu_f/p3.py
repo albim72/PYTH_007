e1437648e70cd45647321447d22a8cf7246e2be1
albim72 <marcin.albiniak72@gmail.com>
2025-09-09 13:57:46 +0200

    Update p3.py

--- DZIEN_2/zlamanie_paradygmatu_f/p3.py
+++ DZIEN_2/zlamanie_paradygmatu_f/p3.py
@@ -1,2 +1,7 @@
+# więzione cienie  - pożne wiązanie w lambdach
 funcs = [lambda: i for i in range(3)]
 print([f() for f in funcs])   # [2, 2, 2]  ← wszystkie patrzą na TEGO SAMEGO i (ostatnią wartość)
+
+#jak bezzpieczniej!
+funcs_fix = [lambda i=i: i for i in range(3)]
+print([f() for f in funcs_fix])
