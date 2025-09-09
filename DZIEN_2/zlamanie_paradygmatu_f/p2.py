4709fb25a0d77944b99fecf6bdc1544a0bf4a35e
albim72 <marcin.albiniak72@gmail.com>
2025-09-09 13:57:02 +0200

    Update p2.py

--- DZIEN_2/zlamanie_paradygmatu_f/p2.py
+++ DZIEN_2/zlamanie_paradygmatu_f/p2.py
@@ -1,3 +1,4 @@
+#przykład 2 - iluzja transformacji
 data = [1, 2, 3, 4]
 
 def double_inplace(xs):
@@ -9,3 +10,18 @@ a = data
 b = double_inplace(data)    # „transformacja”, która zmienia oryginał
 print("a:", a)              # a: [2, 4, 6, 8]  ← oryginał skażony
 print("b:", b)              # b: [2, 4, 6, 8]
+
+#bezpieczniej
+def double_pure(xs):
+    return [x*2 for x in xs]
+
+data = [1, 2, 3, 4]
+c = double_pure(data)
+print(f"data: {data}")
+print(f"c: {c}")
+
+print(sorted(data))
+data.sort(reverse=True)
+print(data)
+data.extend([5,2,7])
+print(data)
