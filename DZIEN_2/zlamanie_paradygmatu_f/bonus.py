3e7e4ae8f7b9d878b66798c062efd61e9615e679
albim72 <marcin.albiniak72@gmail.com>
2025-09-09 13:31:38 +0200

    Create bonus.py

---
+++ DZIEN_2/zlamanie_paradygmatu_f/bonus.py
@@ -0,0 +1,7 @@
+nums = [1, 2, 3, 4]
+itr  = map(lambda x: x * 2, nums)   # iterator, nie lista!
+
+s = sum(itr)                        # pierwszy przebieg ZUŻYWA iterator
+print("suma:", s)                   # 20
+
+print(list(itr))                    # []  ← drugi przebieg nic nie widzi (pustka)
