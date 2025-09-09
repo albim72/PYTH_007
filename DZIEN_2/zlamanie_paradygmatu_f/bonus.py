d0cadc6dca231da82883252b2ff9fbe1fd928221
albim72 <marcin.albiniak72@gmail.com>
2025-09-09 14:03:08 +0200

    Update bonus.py

--- DZIEN_2/zlamanie_paradygmatu_f/bonus.py
+++ DZIEN_2/zlamanie_paradygmatu_f/bonus.py
@@ -1,3 +1,4 @@
+#bonus
 nums = [1, 2, 3, 4]
 itr  = map(lambda x: x * 2, nums)   # iterator, nie lista!
 
@@ -5,3 +6,10 @@ s = sum(itr)                        # pierwszy przebieg ZUŻYWA iterator
 print("suma:", s)                   # 20
 
 print(list(itr))                    # []  ← drugi przebieg nic nie widzi (pustka)
+
+#naprawa
+itr  = list(map(lambda x: x * 2, nums))
+s = sum(itr)                        # pierwszy przebieg ZUŻYWA iterator
+print("suma:", s)                   # 20
+
+print(list(itr)) 
