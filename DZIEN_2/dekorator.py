5790c12f8f8e793072fae07c1743ecdfbc13682c
albim72 <marcin.albiniak72@gmail.com>
2025-09-09 13:25:56 +0200

    Update dekorator.py

--- DZIEN_2/dekorator.py
+++ DZIEN_2/dekorator.py
@@ -1,5 +1,7 @@
 #deklaracja dekoratora
 #przypadek 1: prosty przykład
+import time
+
 def dekorator(funkcja):
     def wrapper():
         print("to jest początek....")
@@ -12,3 +14,26 @@ def powitanie():
     print("witaj w świecie dekoratorów!")
 
 powitanie()
+
+print("_"*80)
+
+def mierzenie_czasu(funkcja):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        wynik = funkcja(*args, **kwargs)
+        end = time.time()
+        print(f"Czas wykonania funkcji to {end-start} sekund.")
+        return wynik
+    return wrapper
+
+@mierzenie_czasu
+def suma_duzej_listy():
+    return sum(range(100_000_000))
+
+print(suma_duzej_listy())
+
+@mierzenie_czasu
+def big_suma():
+    return sum([a+b*c for (a,b,c) in zip(range(100_000_000), range(100_000_000), range(100_000_000))])
+
+print(big_suma())
