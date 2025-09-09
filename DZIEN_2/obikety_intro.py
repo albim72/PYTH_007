47dc892b61706ded744a7278694acdf08e2df3ff
albim72 <marcin.albiniak72@gmail.com>
2025-09-09 14:57:13 +0200

    Update obikety_intro.py

--- DZIEN_2/obikety_intro.py
+++ DZIEN_2/obikety_intro.py
@@ -20,3 +20,36 @@ class Prostokat:
 p = Prostokat(2,3)
 print(p.pole())
 
+#dziedziczenie
+class Zwierze:
+    def __init__(self,imie):
+        self.imie=imie
+
+    def dzwiek(self):
+        return "???"
+
+class Kot(Zwierze):
+    def __init__(self,imie,umaszczenie):
+        super().__init__(imie)
+        self.umaszczenie=umaszczenie
+
+    def dzwiek(self):
+        return "Miau!"
+
+class Pies(Zwierze):
+    def __init__(self,imie,rasa):
+        super().__init__(imie)
+        self.rasa=rasa
+
+    def dzwiek(self):
+        return "Hau!"
+
+pies = Pies("Ludvik","Buldog angielski")
+print(pies.dzwiek())
+
+kot = Kot("Kot","jasne")
+print(kot.dzwiek())
+
+pies = Pies("Roman","Owczarek belgijski")
+print(pies.dzwiek())
+
