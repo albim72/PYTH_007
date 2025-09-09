592e9a43fbfa2d7227558c7954728f0e9c7f92f7
albim72 <marcin.albiniak72@gmail.com>
2025-09-09 10:23:46 +0200

    Update funkcja_intro.py

--- DZIEN_2/funkcja_intro.py
+++ DZIEN_2/funkcja_intro.py
@@ -24,6 +24,18 @@ def powitanie_uzytkownika(imie,jezyk="PL"):
     else:
         print(f"Hello/Witaj {imie}! I'm Python!/Jestem Pythonem!")
 
+#funkcja zwracająca wiele wartości
+def dzielenie(a,b):
+    """
+    zwraca (iloraz całkowity, reszta) dla liczb całkowitych a,b
+    :param a: int
+    :param b: int
+    :return: divmod(a,b)
+    """
+    if b==0:
+        raise ZeroDivisionError("Nie dziel przez zero!")
+    return divmod(a,b)
+
 
 if __name__ == '__main__':
     print("__________ funkcja powitanie ___________")
@@ -43,3 +55,13 @@ if __name__ == '__main__':
     powitanie_uzytkownika("Marek", "PL")
     powitanie_uzytkownika("Anna", "EN")
     powitanie_uzytkownika("Lana", "FR")
+    print("__________ funkcja dzielenie ___________")
+    try:
+        iloraz,reszta = dzielenie(10,5)
+        print(f"dzielenie(10,5) -> iloraz: {iloraz}, reszta: {reszta}")
+        iloraz, reszta = dzielenie(6, 5)
+        print(f"dzielenie(6,5) -> iloraz: {iloraz}, reszta: {reszta}")
+        iloraz, reszta = dzielenie(-3, 0)
+        print(f"dzielenie(-3,0) -> iloraz: {iloraz}, reszta: {reszta}")
+    except ZeroDivisionError as e:
+        print(e)
