48fde29482f0f68ce2317027a2b9a23c8d934f24
albim72 <marcin.albiniak72@gmail.com>
2025-09-10 09:25:39 +0200

    Create bookstore.py

---
+++ DZIEN_3/bookstore.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self, title, author, price, pages, bookstore_nb):
+        self.title = title
+        self.author = author
+        self.price = price
+        self.pages = pages
+        self.bookstore_nb = bookstore_nb
+        self.binding = "miękka"
