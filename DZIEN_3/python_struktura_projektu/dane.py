6d730388be255bf66fc0698b76dfbcc9653d4d63
albim72 <marcin.albiniak72@gmail.com>
2025-09-10 13:07:33 +0200

    Create dane.py

---
+++ DZIEN_3/python_struktura_projektu/dane.py
@@ -0,0 +1,11 @@
+liczby = [4,3,5,26,78,53,-5,34,0,7,-23,53,111,-56]
+
+leader = {
+    "imię": "Jacek",
+    "nazwisko": "Kowalski",
+    "wiek": 46,
+    "miasto": "Warszawa"
+}
+
+MC = "wrzesień"
+kolor = "czerwony"
