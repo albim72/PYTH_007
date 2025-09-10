acd98177ae5b5b50c91a729b530418402d2f3a7d
albim72 <marcin.albiniak72@gmail.com>
2025-09-10 13:23:36 +0200

    Create kwadraty.py

---
+++ DZIEN_3/python_struktura_projektu/mfunkcje/kwadraty.py
@@ -0,0 +1,12 @@
+def kwadrat(lista):
+    return [x**2 for x in lista]
+
+def parzyste(lista):
+    return [x%2==0 for x in lista]
+
+def slownik(dct):
+    return [f"{x}:{y}" for x,y in dct.items()] 
+"""
+dct.items() - pobieranie pary klucz - wartość ze słownika
+przekazywanie pary do pętli element za elementem, następnie wypisanie 
+"""
