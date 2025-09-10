e245204aa35d3f652ad96627a9ed3f15b94e3621
albim72 <marcin.albiniak72@gmail.com>
2025-09-10 13:18:20 +0200

    Create main.py

---
+++ DZIEN_3/python_struktura_projektu/main.py
@@ -0,0 +1,15 @@
+# import dane
+# import dane as dn
+
+from dane import liczby,leader,MC as m,kolor
+
+print("_"*60)
+print(f"liczby -> {liczby}")
+print("_"*60)
+print(f"słownik leader -> {leader}")
+print("_"*60)
+# print(f"stala MC = {MC}")
+print(f"stala MC = {m}")
+MC = "listopad"
+print(f"stala MC = {m}")
+print(f"stala MC = {MC}")
