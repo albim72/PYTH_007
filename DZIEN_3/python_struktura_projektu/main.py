e4c77da0368dea4bb4ec1554e52a6660c1c16844
albim72 <marcin.albiniak72@gmail.com>
2025-09-10 13:39:45 +0200

    Update main.py

--- DZIEN_3/python_struktura_projektu/main.py
+++ DZIEN_3/python_struktura_projektu/main.py
@@ -5,6 +5,8 @@ import copy
 
 from dane import liczby,leader,MC as m,kolor
 from mfunkcje.kwadraty import kwadrat,parzyste,slownik
+from mojpakiet import Osoba
+
 
 print("_"*60)
 print(f"liczby -> {liczby}")
@@ -25,3 +27,6 @@ print(f"kwadrat(lb) -> {kwadrat(lb)}")
 print(f"parzyste(lb) -> {parzyste(lb)}")
 print(f"slownik(leader) -> {slownik(leader)}")
 print("_"*60)
+
+jan = Osoba("Jan",32)
+jan.przedstaw_sie()
