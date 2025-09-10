d1ba885631718a74f9b8e78c8428ac95fe580fa5
albim72 <marcin.albiniak72@gmail.com>
2025-09-10 13:31:09 +0200

    Update main.py

--- DZIEN_3/python_struktura_projektu/main.py
+++ DZIEN_3/python_struktura_projektu/main.py
@@ -1,7 +1,10 @@
 # import dane
 # import dane as dn
+import math
+import copy
 
 from dane import liczby,leader,MC as m,kolor
+from mfunkcje.kwadraty import kwadrat,parzyste,slownik
 
 print("_"*60)
 print(f"liczby -> {liczby}")
@@ -13,3 +16,12 @@ print(f"stala MC = {m}")
 MC = "listopad"
 print(f"stala MC = {m}")
 print(f"stala MC = {MC}")
+print("_"*60)
+print(f"sin(121) = {math.sin(121)}")
+lb = copy.deepcopy(liczby) #kopia głęboka tworzy nowy obiket typu zadanego z uwzględnieniem zgnieżdzeń
+print(f"liczby -> {lb}")
+print(lb is liczby)
+print(f"kwadrat(lb) -> {kwadrat(lb)}")
+print(f"parzyste(lb) -> {parzyste(lb)}")
+print(f"slownik(leader) -> {slownik(leader)}")
+print("_"*60)
