34954c021c41280cd3f3e53ae837cb261acbaf5c
albim72 <marcin.albiniak72@gmail.com>
2025-09-12 10:16:18 +0200

    Update json_file.py

--- DZIEN_5/json_file.py
+++ DZIEN_5/json_file.py
@@ -1,1 +1,26 @@
 
+from __future__ import annotations
+from pathlib import Path
+from typing import Any
+import json
+
+DATA_DIR = Path("data")
+FILE_PATH = DATA_DIR / "filmy.json"
+
+def save_json(path: Path, data: Any) -> None:
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with path.open("w",encoding="utf-8") as f:
+        json.dump(data, f, ensure_accii = False, indent=4)
+
+def load_json(path: Path) -> Any:
+    with path.open("r",encoding="utf-8") as f:
+        return json.load(f)
+
+
+def main():
+    filmy = [
+        {"title":"Nosferatu","year":2024,"genres":["horror",],"duration_min":142},
+        {"title":"Incepcja","year":2010,"genres":["sci-fi","thriller"],"duration_min":156},
+        {"title":"Zimna wojna","year":2018,"genres":["dramat","romans"],"duration_min":143},
+        {"title":"Gladiator II","year":2024,"genres":["historyczny","dramat"],"duration_min":161},  
+    ]
