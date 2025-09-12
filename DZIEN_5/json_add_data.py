32c21e5659245c74201839503db5ee192a751c5c
albim72 <marcin.albiniak72@gmail.com>
2025-09-12 10:29:23 +0200

    Create json_add_data.py

---
+++ DZIEN_5/json_add_data.py
@@ -0,0 +1,24 @@
+import json
+from typing import Any, Dict
+
+def add_to_json(file_path: str, new_data: Dict[str, Any]) -> None:
+    """
+    Dodaje nowe dane do pliku json
+    :param file_path:  ścieżka do pliku JSON
+    :param new_data:  słownik z nowymi danymi do dodania
+    :return: 
+    """
+    
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        data = []
+        
+    if isinstance(data, list):
+        data.append(new_data)
+    else:
+        raise ValueError("Plik JSON nie zawiera listy jako głównej struktury")
+        
+    with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
